d92a627e7def95e8e84f236317ffd30bb728ed6b
abugler <andreas@u.northwestern.edu>
2019-02-03 19:24:11 -0600

    Submitting Assignment 2

--- student_code.py
+++ student_code.py
@@ -128,7 +128,46 @@ class KnowledgeBase(object):
         printv("Retracting {!r}", 0, verbose, [fact_or_rule])
         ####################################################
         # Student code goes here
-        
+
+        if isinstance(fact_or_rule, Fact):
+            for f in self.facts:
+                if f == fact_or_rule:
+                    fact_or_rule = f;
+                    break;
+            fact_or_rule.asserted = False;
+            if not fact_or_rule.supported_by:
+                for fact in fact_or_rule.supports_facts:
+                    fact.supported_by.remove(fact_or_rule);
+                    for support in fact.supported_by:
+                        if isinstance(support, Rule) and match(support.lhs[0], fact_or_rule.statement):
+                            fact.supported_by.remove(support)
+                    if not fact.supported_by and not fact.asserted:
+                        self.kb_retract(fact)
+                for rule in fact_or_rule.supports_rules:
+                    rule.supported_by.remove(fact_or_rule)
+                    for support in rule.supported_by:
+                        if isinstance(support, Rule) and match(support.lhs[0], fact_or_rule.statement):
+                            rule.supported_by.remove(support)
+                    if not rule.supported_by and not rule.asserted:
+                        self.rules.remove(rule)
+                self.facts.remove(fact_or_rule)
+        """
+        for fact in fact_or_rule.supports_facts:
+            fact.supported_by.remove(fact_or_rule)
+            if not fact.supported_by:
+                self.kb_retract(fact)
+        for rule in fact_or_rule.supports_facts:
+            fact.supported_by.remove(fact_or_rule)
+            if not fact.supported_by:
+                self.kb_retract(rule)
+        if isinstance(fact_or_rule, Fact):
+            self.facts.remove(fact_or_rule)
+        if isinstance(fact_or_rule, Rule):
+            self.rules.remove(fact_or_rule)
+        """
+
+
+
 
 class InferenceEngine(object):
     def fc_infer(self, fact, rule, kb):
@@ -140,9 +179,76 @@ class InferenceEngine(object):
             kb (KnowledgeBase) - A KnowledgeBase
 
         Returns:
-            Nothing            
+            Nothing
         """
         printv('Attempting to infer from {!r} and {!r} => {!r}', 1, verbose,
             [fact.statement, rule.lhs, rule.rhs])
         ####################################################
         # Student code goes here
+        bindings = match(fact.statement, rule.lhs[0]);
+        if bindings:
+            """
+            if fact == kb.facts[len(kb.facts)-1]:
+                if len(rule.lhs) <= 1:
+                    newFact = Fact(instantiate(rule.rhs, bindings), [])
+                    newFact.supported_by.append(fact)
+                    newFact.supported_by.append(rule)
+                    fact.supports_facts.append(newFact)
+                    rule.supports_facts.append(newFact)
+                    if newFact not in kb.facts:
+                        kb.facts.append(newFact)
+                    else:
+                        newRule = Rule()
+                        i = 0
+                        for statement in rule.lhs:
+                            if i:
+                                newRule.lhs.append(instantiate(statement, bindings))
+                            i += 1
+                        newRule.rhs = instantiate(rule.rhs, bindings)
+                        newRule.supported_by.append(fact)
+                        newRule.supported_by.append(rule)
+                        fact.supports_rules.append(newRule)
+                        rule.supports_rules.append(newRule)
+                        if newRule not in kb.rules:
+                            kb.rules.append(newRule)
+            elif rule == kb.rules[len(kb.rules)-1]:
+                """
+
+            if len(rule.lhs) == 1:
+                newFact = Fact(instantiate(rule.rhs, bindings), [])
+                if newFact not in kb.facts:
+                    newFact.supported_by.append(fact)
+                    newFact.supported_by.append(rule)
+                    newFact.asserted = False
+                    fact.supports_facts.append(newFact)
+                    rule.supports_facts.append(newFact)
+                    printv('{!r}', 1, verbose, [newFact.statement])
+                    kb.facts.append(newFact)
+                    for r in kb.rules:
+                        self.fc_infer(newFact, r, kb)
+            else:
+                newRule = Rule('rule', [])
+                i = 0
+                for statement in rule.lhs:
+                    if i == 1:
+                        newRule.lhs[0] = instantiate(statement, bindings)
+                    elif i:
+                        newRule.lhs.append(instantiate(statement, bindings))
+                    i += 1
+                newRule.rhs = instantiate(rule.rhs, bindings)
+                if newRule not in kb.rules:
+                    newRule.supported_by.append(fact)
+                    newRule.supported_by.append(rule)
+                    newRule.asserted = False
+                    fact.supports_rules.append(newRule)
+                    rule.supports_rules.append(newRule)
+                    printv('{!r}=>{!r}', 1, verbose, [newRule.lhs, newRule.rhs])
+                    kb.rules.append(newRule)
+                    for f in kb.facts:
+                        self.fc_infer(f, newRule, kb)
+
+
+
+
+
+
